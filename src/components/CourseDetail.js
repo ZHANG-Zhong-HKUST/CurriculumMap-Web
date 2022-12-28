18fc534ffabe55a149a83ce149244f048d758ebd
ZHANG-Zhong-HKUST <zzhangfc@connect.ust.hk>
2022-12-28 16:18:12 +0800

    add links to other web

--- src/components/CourseDetail.js
+++ src/components/CourseDetail.js
@@ -5,7 +5,7 @@ function CourseDetail(props) {
     if(courses[code]==undefined) {
         return (
             <div>
-                <p>The Curriculum map project is first introduce by Zory in <a href="https://github.com/zory233/CurriculumMap">their COMP2012H course project</a>.</p>
+                <p>The Curriculum map project is first introduce by Zory & Wang in <a href="https://github.com/zory233/CurriculumMap">their COMP2012H course project</a>.</p>
                 <p>This is a web-based re-implementation. Please check the above link for the desktop APP, which has more functionality.</p>
                 <p>The code of this web-app is available on <a href="https://github.com/ZHANG-Zhong-HKUST/CurriculumMap-Web">github</a>.</p>
             </div>
@@ -26,6 +26,9 @@ function CourseDetail(props) {
             <p className="">{courses[code].tcov}</p>
             <b>Description:</b>
             <p className="">{courses[code].des}</p>
+            <a href={"https://ust.space/review/"+code} target="_blank" rel="noopener noreferrer">Link to USTSPACE</a>
+            <a href={"https://ug-msss.hkust.edu.hk/cwiki_catalog/"+code} target="_blank" rel="noopener noreferrer">Link to Cwiki</a>
+            <a href={"https://prog-crs.hkust.edu.hk/ugcourse/2022-23/search?keyword="+code} target="_blank" rel="noopener noreferrer">Link to HKUST course catalog</a>
         </div>
     );
 }
